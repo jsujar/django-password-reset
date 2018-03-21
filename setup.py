a5d16129c6b57f408fe24dd5012759518bbdeed7
Javier Sújar <javier.sujar@exo.works>
2018-03-21 17:45:47 +0100

    Updated test for Django2.0.3

--- setup.py
+++ setup.py
@@ -19,16 +19,13 @@ setup(
     description='Class-based views for password reset.',
     long_description=open('README.rst').read(),
     install_requires=[
-        'Django>=1.8',
+        'Django>=2.0.3',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
-        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0.3',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
